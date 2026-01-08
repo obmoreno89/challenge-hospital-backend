d1c239c63ecd2389a9aa0136d8a78da1d9adbc4a
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2026-01-07 18:53:20 -0600

    FEATURES - se crea metodo delete para borrar registros

--- app/services/delete_ticket.py
+++ app/services/delete_ticket.py
@@ -2,13 +2,22 @@ from sqlmodel import Session
 from app.schemas import TicketTable
 
 
+from sqlmodel import Session, select
+from app.schemas import TicketTable
+
 def delete_ticket_by_id(session: Session, ticket_id: int) -> bool:
-    statement = session.query(TicketTable).where(TicketTable.id == ticket_id)
-    ticket = session.exec(statement).first()
-    
+    statement = select(TicketTable).where(TicketTable.id == ticket_id)
+    result = session.exec(statement)
+    ticket = result.first()
+
     if not ticket:
         return False
-    
-    session.delete(ticket)
-    session.commit()
-    return True
+
+    try:
+        session.delete(ticket)
+        session.commit()
+        return True
+    except Exception as e:
+        session.rollback()
+        print(f"Error al eliminar: {e}")
+        return False
