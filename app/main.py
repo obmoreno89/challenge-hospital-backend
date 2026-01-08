3b1909978f0188215ac33e43b79cd3d0efc5b447
Omar Barragan Moreno <omar@Omars-MacBook-Air.local>
2026-01-08 01:17:24 -0600

    FEATURES - se agrega configuracion para el tema de CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 from sqlmodel import SQLModel
@@ -8,6 +9,19 @@ from app.schemas import ticket_model
 from app.api import router
 
 app = FastAPI(title="{URL}/hospital/v1/operaciones/tickets", version="1.0.0", description="API para hacer operaciones nivel CRUD", swagger_ui_parameters={"operationsSorter": "method"}, openapi_url="/openapi.json", docs_url="/docs")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(HTTPException)
 async def custom_http_exeption_handler(request: Request, exc: HTTPException):
